13d0d0b8b51fc7e9a0ba87740e5a8571bad51cae
vladaklokk <vladagperfect@gmail.com>
2024-03-22 18:20:37 +0200

    last commit

--- src/hooks/useContacts.js
+++ src/hooks/useContacts.js
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { ContactsContext } from "../contexts/ContactsContext";
-
 export function useContacts() {
     const context = useContext(ContactsContext);
     return context
